b64ad0a6c691f1fc6f5f15210ef65dd9f9aec149
Marcin Kunert <the.martines@gmail.com>
2022-05-05 12:57:52 +0200

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,16 @@
 package pl.javastart.task;
 
-public class Main {
-
+class Main{
     public static void main(String[] args) {
+        String product1Name = "Mleko";
+        String producer1Name = "Mlekowita";
+        double price1 = 2.5;
 
+        String product2Name = "Czekolada";
+        String producer2Name = "Wedel";
+        double price2 = 2.19;
+
+        System.out.println(product1Name + " "  + producer1Name + " "  + price1 + "zł");
+        System.out.println(product2Name + " " + producer2Name + " " + price2 + "zł");
     }
 }
